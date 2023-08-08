8af0a52e733e496b2568684a564f5d5c1b321d41
ridwan fansuri <ridwan.fansuri@dana.id>
2023-08-08 21:11:23 +0700

    initial commit

---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Head() {
+  return (
+    <>
+    <title>Google Clone Next js 13</title>
+    <meta content='width=device-width, initial-scale-1' name='viewport' />
+    <meta name='description' content='Google clone craeted by Next JS 23'/>
+    <link rel="icon" href="/favicon.svg" />
+    </>
+  )
+}
